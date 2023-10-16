586dbe13797a95395d44f1ebeca9b674f313c639
Jorge <jorge.garcia.pastor@gmail.com>
2023-10-16 10:37:30 +0200

    NEW create project

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Nav = ()=>{
+    return <>
+        <h1 className='text-teal-300'>
+            estoy aqui en el nav
+        </h1>
+        <picture>
+        <img src='./src/assets/img/logos/decampo_alt.svg'/>
+        </picture>
+        
+        </>
+}
+export default Nav
