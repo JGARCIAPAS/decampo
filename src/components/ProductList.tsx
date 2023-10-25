4a24ec48c262c59cc1057aa9f52419fec87c8d91
Jorge <jorge.garcia.pastor@gmail.com>
2023-10-25 17:14:55 +0200

    FIX adding styles to products

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -20,7 +20,7 @@ const ProductList = () => {
   }, [formattedCategory]);
   console.log(categoryData);
   return (
-    <div className="w-9/12 mx-auto ">
+    <div className="w-9/12 mx-auto font-sanamedium">
       <div className="breadcrumbs pt-2.5">
         <Link to="/">Inicio</Link>
         {">"}
@@ -29,27 +29,40 @@ const ProductList = () => {
       <div className="bg-slate-300 my-5">100 productos </div>
       <div>
         {categoryData && Object.keys(categoryData).length > 0 ? (
-          <ul className="grid gap-4 grid-cols-5">
+          <ul className="grid gap-4 grid-cols-6">
             {Object.keys(categoryData).map((item) => {
               const product = categoryData[item];
               return (
                 <li className="border border-slate-300 p-2.5" key={item}>
                   <img
-                    className="w-[130px] mx-auto"
+                    className="w-[130px] mx-auto mb-5"
                     src={`./src/assets/img/productos/${category}/${product.img}`}
                     alt={product.nombre}
                   />
-                  <h3>{product.nombre}</h3>
-                  <p>Unidad</p>
-                  <p className="flex border border-red">
-                    <span className="first-letter:text-xl">
+                  <h3 className="text-sm text-slate-500 uppercase">
+                    {product.nombre}
+                  </h3>
+                  <p className="text-xs">Unidad</p>
+                  <p className="flex border border-red w-fit px-5 py-2.5">
+                    <span className="first-letter:text-2xl font-bold font-sanablackregular text-red">
                       {product.precio}
                     </span>
                     <img
-                      className="w-3 h-3 mt-1.5"
-                      src="./src/assets/img/icons/pokedollar.svg"
+                      className="w-3 h-3 mt-3 ml-1"
+                      src="./src/assets/img/icons/pokedollar_red.svg"
                     />
                   </p>
+                  <div className="mt-2.5 flex justify-between">
+                    <img
+                      className=""
+                      src="./src/assets/img/icons/heart.svg"
+                      alt="fav"
+                    />
+                    {/* TODO: MODAL TO THE PRODUCT */}
+                    <p className="bg-red text-white font-bold w-fit py-1.5 px-6 rounded-3xl text-sm">
+                      Comprar
+                    </p>
+                  </div>
                 </li>
               );
             })}
